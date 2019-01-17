13a6a35d4e7a4f73aea6ca25bb7ebf9f77910187
msato0731 <rhcq.3127@gmail.com>
2019-01-17 00:09:16 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -55,7 +55,7 @@ class UsersController extends Controller
           'users' => $followers,
         ];
         
-        $data += $this->count($user);
+        $data += $this->counts($user);
         
         return view('users.followers', $data);
     }
